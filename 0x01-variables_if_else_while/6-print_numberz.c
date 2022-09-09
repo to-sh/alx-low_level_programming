b834d202ca74997a2413b6be4e3428eceb5dae50
to-sh <antonybahati79@gmail.com>
2022-09-09 08:26:04 -0700

    task six

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,14 +7,13 @@
   */
 int main(void)
 {
-int n;
+»       int n;
 
-for (n = 0; n <= 9; n++)
+»       for (n = 0; n <= 9; n++)
 {
-putchar(n);
+»       putchar(n);
 }
+»       putchar('\n');
 
-putchar('\n');
-
-return (0);
+»       return (0);
 }
