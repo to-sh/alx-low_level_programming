d15cf7846095ace2164ea66836b46a2e1f62cdd4
to-sh <antonybahati79@gmail.com>
2022-09-13 17:37:02 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,32 +1,11 @@
-#include <stdio.h>
-
+#include "holberton.h"
 /**
- * main - Prints all possible combinations of two different digits,
- *        in ascending order, separated by a comma followed by a space.
- *
- * Return: Always 0.
+ * print_times_table - Entry point
+ * @n: input
+ * Return: Always 0 (Success)
  */
-int main(void)
+void print_times_table(int n)
 {
-	int digit1, digit2;
-
-	for (digit1 = 0; digit1 < 9; digit1++)
-
-	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
-
-			if (digit1 == 8 && digit2 == 9)
-				continue;
-
-			putchar(',');
-			putchar(' ');
-		}
-	}
-
-	putchar('\n');
 
 	return (0);
 }
